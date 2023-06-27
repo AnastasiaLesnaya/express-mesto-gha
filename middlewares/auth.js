5514c0ccf7befce6ece9e3228a280f5520921f00
Anastasia Lesnaya <lesnyasha@yandex.ru>
2023-06-27 22:44:28 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const Unauthorized = require('../utils/errors/Unauthorized');
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
